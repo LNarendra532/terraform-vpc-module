101c7527ff97a78f5373230e7cf0440febad60a9
Naren <lnaren532@gmail.com>
2025-06-17 17:24:51 +0530

    terraform_vpc_module_

---
+++ vpc.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = "default"
+  enable_dns_hostnames ="true"
+
+  tags = merge(
+
+    local.common_tags, {
+        Name =  "${var.project}-${var.environment}" #interpolation
+    }
+  )
+  
+}
+#roboshop-dev
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+
+    local.common_tags, {
+        Name =  "${var.project}-${var.environment}" #interpolation
+    }
+  )
+
+}
+#roboshop-dev-public-use-east-1
+resource "aws_subnet" "public" {
+    count = length(var.public_subnet_cidrs)
+    vpc_id     = aws_vpc.main.id
+    cidr_block = var.public_subnet_cidrs[count.index]
+    availability_zone = local.az_zones[count.index]
+    map_public_ip_on_launch = true
+
+   tags = merge(
+
+    local.common_tags, {
+        Name =  "${var.project}-${var.environment}-public-${local.az_zones[count.index]}" #interpolation
+    }
+  )
+}
